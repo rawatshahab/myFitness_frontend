2bab9229e3c4eab024dd1e9f63ef6989c78ee0e1
Mayank Rawat <131944775+rawatshahab@users.noreply.github.com>
2023-12-17 23:31:03 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,7 +35,7 @@ const Navbar = () => {
     >
           
 <Link to="/">
-<img src={Logo} alt="logo" style={{width:'270px', margin:'5px -1041px'}} />
+<img src={Logo} alt="logo" style={{width:'270px', margin:'5px -1130px'}} />
 </Link>     
 {/* <Link style={{textDecoration: 'none',color:'white'}}>Hello</Link> */}
 
